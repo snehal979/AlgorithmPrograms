08f219a0c977637806318e81aebb6e2c838e61eb
Snehal979 <bansodsnehal535@gmail.com>
2022-11-28 22:36:16 +0530

    (Snehal)Add: prime number between range

---
+++ AlgorithmPrograms/AlgorithmPrograms/PrimeNumber.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    public class PrimeNumber
+    {
+        LinkedList<int> sorted = new LinkedList<int>();
+        public void TotalPrimeNumber(int[] arr)
+        {
+
+            int i;
+            try
+            {
+                for (i = 0; i<=arr.Length-1; i++)
+                {
+                    arr[i] = i;
+                    int count = 0;
+                    for (int j = 1; j <= i; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            count++;
+
+                        }
+                    }
+                    if (count == 2)
+                    {
+                        Console.WriteLine(i);
+                        sorted.AddFirst(i);
+
+
+                    }
+
+                }
+               
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+
+
+        }
+    }
+}
--- AlgorithmPrograms/AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/AlgorithmPrograms/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("AlgorithmPrograms");
             Console.WriteLine("1.binary word seraech from data \n 2. guessnumber \n 3.userinput check \n4.insertionSort" +
-                "\n 5.bubble sort \n 6.merge sort \n 7. anagram");
+                "\n 5.bubble sort \n 6.merge sort \n 7. anagram \n 8.prime number between range");
             int num = Convert.ToInt32(Console.ReadLine());
             int[] arr = { 1, 5, 2, 8, 5, 7, 9, 2 };
             switch (num)
@@ -50,7 +50,11 @@
                     AnaGram anaGram = new AnaGram();
                     anaGram.checkAnagram("earth", "heart");
                     break;
-
+                case 8:
+                    PrimeNumber primeNumber = new PrimeNumber();
+                    int[] arr1 = new int[20];
+                    primeNumber.TotalPrimeNumber(arr1);
+                    break;
 
 
 
