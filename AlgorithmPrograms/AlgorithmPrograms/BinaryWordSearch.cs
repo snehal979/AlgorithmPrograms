2d0839494cb7a4d9fdd2aa4db3731b7de308e1cb
Snehal979 <bansodsnehal535@gmail.com>
2022-11-28 22:00:09 +0530

    (Snehal)Add:Uc1 binary search from file

---
+++ AlgorithmPrograms/AlgorithmPrograms/BinaryWordSearch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    internal class BinaryWordSearch
+    {
+        string[] array;
+        public void ReadFile(string filepath)
+        {
+            string words = File.ReadAllText(filepath);
+            array=words.Split(",");
+        }
+
+        public void Display()
+        {
+            foreach (var data in array)
+            {
+                Console.WriteLine(data);
+            }
+        }
+        // for search word method
+        public void SearchWord(string input)
+        {
+            foreach (var word in array)
+            {
+                if (word.Equals(input))
+                {
+                    Console.WriteLine("found that word in the list using binarry serach");
+                    return;
+                }
+
+            }
+            Console.WriteLine("search is not found in the first at word");
+        }
+    }
+}
--- AlgorithmPrograms/AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/AlgorithmPrograms/Program.cs
@@ -5,6 +5,22 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("AlgorithmPrograms");
+            Console.WriteLine("1.binary word seraech from data");
+            int num = Convert.ToInt32(Console.ReadLine());
+            switch (num)
+            {
+                case 1:
+                    string filepath = "C:\\Users\\hp\\Desktop\\newFolder\\AlgorithmPrograms\\AlgorithmPrograms\\AlgorithmPrograms\\bInarysearchdata.txt";
+
+                    BinaryWordSearch binaryWordSearch = new BinaryWordSearch();
+                    binaryWordSearch.ReadFile(filepath);
+                    binaryWordSearch.Display();
+                    string word = Console.ReadLine();
+                    binaryWordSearch.SearchWord(word);
+
+                    break;
+            }
+            
         }
     }
 }
