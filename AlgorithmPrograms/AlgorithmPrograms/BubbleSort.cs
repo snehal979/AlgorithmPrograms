5f5fe54524625b813e183564bbfcce50c78d1199
Snehal979 <bansodsnehal535@gmail.com>
2022-11-28 22:20:39 +0530

    (Snehal)Add: array sort by bubble sort

---
+++ AlgorithmPrograms/AlgorithmPrograms/BubbleSort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    public class BubbleSort
+    {
+        public void Sort(int[] arr)
+        {
+            for (int i = 0; i<arr.Length-1; i++)
+            {
+                for (int j = 0; j<arr.Length-1; j++)
+                {
+                    if (arr[j].CompareTo(arr[j+1])>0)
+                    {
+                        int temp = arr[j+1];
+                        arr[j+1] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            foreach (var data in arr)
+            {
+                Console.WriteLine(data);
+            }
+        }
+    }
+}
--- AlgorithmPrograms/AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/AlgorithmPrograms/Program.cs
@@ -5,8 +5,10 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("AlgorithmPrograms");
-            Console.WriteLine("1.binary word seraech from data \n 2. guessnumber \n 3.userinput check \n4.insertionSort");
+            Console.WriteLine("1.binary word seraech from data \n 2. guessnumber \n 3.userinput check \n4.insertionSort" +
+                "\n 5.bubble sort");
             int num = Convert.ToInt32(Console.ReadLine());
+            int[] arr = { 1, 5, 2, 8, 5, 7, 9, 2 };
             switch (num)
             {
                 case 1:
@@ -28,11 +30,14 @@
                     userInputCheck.ReplaceString();
                     break;
                 case 4:
-                    int[] arr = { 1, 5, 2, 8, 5, 7, 9, 2 };
+                  
                     InsertionSort insertionSort = new InsertionSort();
                     insertionSort.Sort(arr);
                     break;
-
+                case 5:
+                    BubbleSort bubbleSort = new BubbleSort();
+                    bubbleSort.Sort(arr);
+                    break;
 
 
 
